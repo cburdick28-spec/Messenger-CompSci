7bceeffd5a27113ad03883dc00804fad845b4aec
Connor <cburdick28@brewstermadrid.com>
2026-04-23 11:22:05 +0200

    Update config.js

--- static/config.js
+++ static/config.js
@@ -1,4 +1,4 @@
 window.MESSENGER_CONFIG = {
-  supabaseUrl: ""https://hnjziardboghvhyhyhcx.supabase.co",
+  supabaseUrl: "https://hnjziardboghvhyhyhcx.supabase.co",
   supabaseAnonKey: "sb_publishable_l13qSZTD_O11G6cSkfLS_w_1ej0ckZy"
 };
