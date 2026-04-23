f1ef80690811757f65f1072a84876a767fbecf54
Connor <cburdick28@brewstermadrid.com>
2026-04-23 11:09:14 +0200

    Update to start website

--- static/config.js
+++ static/config.js
@@ -1,4 +1,4 @@
 window.MESSENGER_CONFIG = {
-  supabaseUrl: "https://ooxhxlpkfhqjbnlpponp.supabase.co",
-  supabaseAnonKey: "sb_publishable_EawsanVdodBiF_M6hrJMJw_B50sealD"
+  supabaseUrl: "https://supabase.com/dashboard/project/hnjziardboghvhyhyhcx",
+  supabaseAnonKey: "vck_1GNkPgzCiWASTHH9igGBhTzSbJLutkA6XUUQTOKquLEws72uvp4K9eLn"
 };
